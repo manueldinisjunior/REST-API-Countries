cbf89507dc7b47a0dd0a5638c3e019d690f61fea
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2024-02-13 22:14:19 +0200

    #

--- src/loader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Loader = () => {
-  return (
-    <div className='dark:bg-darkMode bg-lightMode h-[100vh] grid place-items-center dark:text-light-dark-gray text-light-very-dark-blue'>Loading...</div>
-  )
-}
-
-export default Loader
--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { router } from '@/lib/router';
-import '@/styles/global.css';
-
-const root = createRoot(document.getElementById('root')!);
-
-root.render(<RouterProvider router={router} />);
